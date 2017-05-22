1ed52e8deccca484d29413f8fbfb4c6cf03435a3
Chris Amico <eyeseast@gmail.com>
2017-05-22 16:56:54 -0400

    Switch to es6 imports

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
-var d3 = require('d3');
-var topojson = require('topojson');
+import {select, selectAll} from 'd3-selection';
+import {json} from 'd3-request';
+import {geoPath, geoMercator} from 'd3-geo';
+import * as topojson from 'topojson';
 
-var width = parseInt(d3.select('#map').style('width'), 10);
+var width = parseInt(select('#map').style('width'), 10);
 var height = width * .5;
 var towns;
 
-var projection = d3.geoMercator()
+var projection = geoMercator()
     .center([-71.3824374, 42.4072107])
     .scale(width * 10)
     //.translate([width / 2, height / 2]);
 
-var path = d3.geoPath()
+var path = geoPath()
     .projection(projection);
 
-var map = d3.select('#map').append('svg')
+var map = select('#map').append('svg')
     .style('width', width + 'px')
     .style('height', height + 'px');
 
-d3.json('data/towns.json', function(err, data) {
+json('data/towns.json', function(err, data) {
     if (err) throw err;
     
     window.data = data;
