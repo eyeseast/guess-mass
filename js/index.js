970b7e9ad5857f25c94933389521176b32297ebb
Chris Amico <eyeseast@gmail.com>
2017-05-23 16:12:24 -0400

    Autoscale MA

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import Map from './components/Map.html';
+import * as topojson from 'topojson';
 
 const map = new Map({
     target: document.querySelector('#map')
 });
 
 window.map = map;
+window.topojson = topojson;
